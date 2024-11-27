25fc95add3da22b75a9b8007ad60fd180c05cfef
sefa-unal <sefa@alvaindustries.com>
2024-11-27 14:18:28 +0100

    update styling in serialparser.py

--- serialparser.py
+++ serialparser.py
@@ -10,11 +10,11 @@ import time
 class Endianness:
     LITTLE  = 0
     BIG     = 1
-    
+
     def getParserChar(self, aEndianness):
         lParserChar = ['<', '>']
         return lParserChar[aEndianness]
-    
+
 class DataType:
     INT8    = 0
     UINT8   = 1
@@ -26,23 +26,23 @@ class DataType:
     UINT64  = 7
     FLOAT   = 8
     DOUBLE  = 9
-    
+
     def getSize(self, aDataType):
         lDataSize = [1, 1, 2, 2, 4, 4, 8, 8, 4, 8]
         return lDataSize[aDataType]
-    
+
     def getParserChar(self, aDataType):
         lParserChar = ['b', 'B', 'h', 'H', 'l', 'L', 'q', 'Q', 'f', 'd']
         return lParserChar[aDataType]
 
 class SerialParser:
-    def __init__(self, aStartSequence, 
-                 aDataType:DataType, 
-                 aNumChannel, 
+    def __init__(self, aStartSequence,
+                 aDataType:DataType,
+                 aNumChannel,
                  aEndianness:Endianness = Endianness.LITTLE,
                  aEndSequence = [],
                  aEnableDebug = 0):
-        
+
         self.buffer             = bytearray()
         self.debug              = aEnableDebug
         self.setParserScheme(aStartSequence, aDataType, aNumChannel, aEndianness, aEndSequence)
@@ -52,29 +52,28 @@ class SerialParser:
         self.parserErrCount     = 0
         self.parserErrRate      = 0
 
-    def setParserScheme(self, aStartSequence, 
-                        aDataType:DataType, 
-                        aNumChannel, 
-                        aEndianness:Endianness = Endianness.LITTLE, 
+    def setParserScheme(self, aStartSequence,
+                        aDataType:DataType,
+                        aNumChannel,
                         aEndSequence = []):
-        
+
         self.dataType           = aDataType
         self.numChannels        = aNumChannel
         self.startSequence      = aStartSequence
+        self.checkSum           = aCheckSum
         self.endSequence        = aEndSequence
         self.endianness         = aEndianness
-        
+
         self.payloadSize        = self.numChannels * DataType().getSize(self.dataType)
         self.headerSize         = len(self.startSequence)
-        self.packetSize         = self.headerSize + self.payloadSize + len(self.endSequence)
-        
+
         self.parserString       = Endianness().getParserChar(self.endianness)
         for i in range(self.numChannels):
             self.parserString += DataType().getParserChar(self.dataType)
-        
+
     def getPacketRate(self):
         return self.packetRate
-    
+
     def getErrorRate(self):
         return self.parserErrRate // self.packetSize
 
@@ -85,11 +84,11 @@ class SerialParser:
             explst.append('XX')
         explst.extend(self.endSequence)
         return str(explst)
-        
+
     def parse(self, data):
         parsedPackets = []
         self.buffer.extend(data)
-        
+
         while len(self.buffer) >= self.packetSize:
             lNotFound = 0
             # search for start sequence
@@ -97,34 +96,34 @@ class SerialParser:
                 if self.buffer[i] != val :
                     lNotFound = 1
                     break
-            
+
             if lNotFound:
                 # remove a byte and search again
                 self.buffer.pop(0)
                 self.parserErrCount += 1
                 continue
-            
+
             # search for end sequence
             for i, val in enumerate(self.endSequence):
-                if self.buffer[i + self.headerSize + self.payloadSize] != val :
+                if self.buffer[i + self.headerSize + self.payloadSize + self.checkSumSize] != val:
                     lNotFound = 1
                     break
-                
+
             if lNotFound:
                 # remove a byte and search again
                 self.buffer.pop(0)
                 self.parserErrCount += 1
                 continue
-            
+
             # found a valid packet
             byteRange = self.buffer[self.headerSize:self.headerSize + self.payloadSize]
             parsedValues = struct.unpack(self.parserString, byteRange)
             parsedPackets.append(parsedValues)
-            
+
             # remove parsed packet from buffer
             self.buffer = self.buffer[self.packetSize:]
 
-        # calculate incoming packet/error rate 
+        # calculate incoming packet/error rate
         self.packetCount += len(parsedPackets)
         curTime = time.perf_counter()
         if self.startTime == 0:
@@ -136,9 +135,9 @@ class SerialParser:
                 self.packetCount = 0;
                 self.parserErrRate = self.parserErrRate * 0.3 + (self.parserErrCount / timeDelta) * 0.7
                 self.parserErrCount = 0
-                
+
                 self.startTime = curTime
-                
+
         # Transpose of parsedPackets
         parsedPackets = list(map(list, zip(*parsedPackets)))
         return parsedPackets

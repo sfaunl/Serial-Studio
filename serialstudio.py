efcdfa329d5a144bd41920d373a3ae8d1bf2142d
sefa-unal <sefa@alvaindustries.com>
2024-12-18 02:36:10 +0100

    chore: update version to 0.2.6 and add new features including cobs, discard bytes, value view, enable fft, plot name and color support

--- serialstudio.py
+++ serialstudio.py
@@ -14,6 +14,7 @@ Created on Sat Apr 23 02:08:23 2022
 @since		v0.2.3 : migrate from pyqt to pyside
 @since      v0.2.4 : add crc and channel name support
 @since      v0.2.5 : use new color palette
+@since      v0.2.6 : add cobs, discard bytes, value view, enable fft, plot name and color support
 """
 
 from PySide6.QtWidgets import (
@@ -60,7 +61,7 @@ class ConfigParser():
 
 class SerialStudio(QMainWindow):
     appname = "Serial Studio"
-    version = "0.2.5"
+    version = "0.2.6"
 
     colorPalette = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9']
     defaultParams = {

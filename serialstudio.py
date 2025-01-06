b3140825455aa535aac568973ecd39e161d77c0b
sefa-unal <sefa@alvaindustries.com>
2025-01-06 11:23:28 +0100

    refactor: update version to 0.3.4 and set channels to be collapsed by default

--- serialstudio.py
+++ serialstudio.py
@@ -5,7 +5,7 @@ Created on Sat Apr 23 02:08:23 2022
 @brief      Serial data visualizer
 @author     Sefa Unal
 
-@version    0.3.3
+@version    0.3.4
 @date       06/01/2025
 @since		v0.1 : initial release
 @since		v0.2 : add multiplier and offset
@@ -21,6 +21,7 @@ Created on Sat Apr 23 02:08:23 2022
 @since      v0.3.1 : add channel expand/collapse button
 @since      v0.3.2 : use ctrl+c signal to terminate the application
 @since      v0.3.3 : add reporting of raw packet data to display in hex format
+@since      v0.3.4 : channels should be collapsed by default
 """
 
 from PySide6.QtWidgets import (
@@ -74,7 +75,7 @@ class ConfigParser():
 
 class SerialStudio(QMainWindow):
     appname = "Serial Studio"
-    version = "0.3.3"
+    version = "0.3.4"
 
     colorPalette = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9']
     defaultParams = {
@@ -936,7 +937,7 @@ class SerialStudio(QMainWindow):
                     if ch >= numchan:
                         self.paramChannels.removeChild(self.paramChannels.child(("Channel_{0}".format(ch))))
                     elif ch >= childcount:
-                        newChannel = self.paramChannels.addChild({'name': "Channel_{}".format(ch), 'title': chtitle, 'type': 'bool', 'value': chenabled})
+                        newChannel = self.paramChannels.addChild({'name': "Channel_{}".format(ch), 'title': chtitle, 'type': 'bool', 'expanded': False, 'value': chenabled})
                         newChannel.addChild({'name': 'Name', 'type': 'str', 'value': chtitle})
                         newChannel.addChild({'name': 'Color', 'type': 'color', 'value': chcolor})
                         newChannel.addChild({'name': 'Value', 'type': 'str', 'value': "", 'readonly': True})
